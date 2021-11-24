31de12860c64261c064b6dc1c4fb56acd77f7b9f
abhi1711-droid <abhisheksinha1709@gmail.com>
2021-11-25 01:32:56 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return(
+    <>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more than this React app.</p>
+    <small>HiAbhi</small>
+    </>
+  );
+};
+export default App;
